c0bbc3147b09d588ac264d11aadc33c7fb7f82a9
Adrian Cowham <adrian.cowham@gmail.com>
2024-02-01 19:26:14 -0800

    fixing typo

--- Chatbot.py
+++ Chatbot.py
@@ -140,7 +140,7 @@ st.markdown(
 ## Do Things That Don't Scale
 By Paul Graham
 
-Ask PG a question, then asking a similar one to see how the cache works.
+Ask PG a question, then ask a similar one to see how the cache works.
 Or ask him a dissimilar one, I don't care.
 
 Shout out to PG for writing this essay.
